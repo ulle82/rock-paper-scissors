6300851699a7b93953aabb9dff2142e45aafc66e
Ulrik Wichmann <28593903+ulle82@users.noreply.github.com>
2026-02-27 16:15:19 +0100

    Finished Rock, paper, Scissors project. Only works in console

---
+++ scripts/script.js
@@ -0,0 +1,64 @@
+let humanScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let computerSelection = Math.floor(Math.random() * 3);
+    // console.log(choices[computerSelection]);
+    return choices[computerSelection];
+}
+
+function getHumanChoice() {
+    let playerSelection = prompt("Please enter: Rock, Paper or Scissors");
+    playerSelection = playerSelection.toLowerCase();
+    // console.log(playerSelection);
+    return playerSelection;
+}
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`The computer chose ${computerChoice} and you chose ${humanChoice}`);
+    if (humanChoice == "rock" && computerChoice == "rock") {
+        console.log(`It's a draw!`);
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log(`It's a draw!`);
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        console.log(`It's a draw!`);
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+        ++humanScore;
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log(`You lose, ${humanChoice} is beaten by ${computerChoice}!`);
+        ++computerScore;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+        ++humanScore;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log(`You lose, ${humanChoice} is beaten by ${computerChoice}!`);
+        ++computerScore;
+    }  else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+        ++humanScore;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log(`You lose, ${humanChoice} is beaten by ${computerChoice}!`);
+        ++computerScore;
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+        if (i == 4) {
+            if (humanScore > computerScore) {
+                console.log(`Congratulation, you win! Your score is ${humanScore} and the computers is ${computerScore}.`);
+            } else if (computerScore > humanScore) {
+                console.log(`You lose! Your score is ${humanScore} and the computers is ${computerScore}. Better luck next time!`);
+            } else {
+                console.log(`Its a draw! You both scored ${humanScore} points`);
+            }
+            
+        }
+    }
+}
+
+playGame()
+
